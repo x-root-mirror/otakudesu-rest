e7b8f75d5c844d446eb9ef28bea2688c812a8446
x-root-mirror <yutubku456@gmail.com>
2021-03-05 06:43:35 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const router = require('./routes/index')
+const cors = require('cors')
+
+app.use(cors())
+
+app.use('/api',router)
+app.use('/',(req,res)=>{
+    res.send({
+        message : 'Welcome To Unofficial Otakudesu Rest Api',
+        createdBy : 'KaedeNoKi Team ♥️'
+    })
+})
+app.use('/api',(req,res) =>{
+    res.send({
+        message:'check our github for more info',
+        github :'https://github.com/Kaede-No-Ki/otakudesu-rest-api'
+    })
+})
+
+
+app.use('*',(req,res) =>{
+    res.json({
+        'status':'not found path',
+        message: 'read the docs here https://github.com/Kaede-No-Ki/otakudesu-rest-api'
+    })
+})
+app.listen(port, () => {
+    console.log('listening on port', port)
+})
